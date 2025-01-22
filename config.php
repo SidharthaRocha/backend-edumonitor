3839e8719a58bf20106b39654c7d57dddd9f3186
Max Sidhartha Souza Rocha <max_rocha88@hotmail.com>
2025-01-22 15:08:09 -0300

    atualização 5 edumonitor

--- config.php
+++ config.php
@@ -1,22 +1,20 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';  // Carrega as bibliotecas do Composer
-
-// Carrega o arquivo .env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-// Obter as variáveis de ambiente
-$servername = $_ENV['MYSQLHOST'];   // Host do banco de dados
-$username = $_ENV['MYSQLUSER'];     // Usuário do banco de dados
-$password = $_ENV['MYSQLPASSWORD']; // Senha do banco de dados
-$dbname = $_ENV['MYSQLDATABASE'];   // Nome do banco de dados
+$servername = "junction.proxy.rlwy.net"; // Host do banco de dados
+$username = "root";                      // Usuário do banco de dados
+$password = "BHfvIHTDwRyggmmHaNeFyTjJTwSexvum"; // Senha do banco de dados
+$dbname = "railway";                     // Nome do banco de dados
 
 try {
     // Criar a conexão com PDO
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+
+    // Configurar o modo de erro do PDO para exceções
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Se a conexão for bem-sucedida
     echo "Conectado com sucesso!";
 } catch (PDOException $e) {
+    // Se houver erro na conexão
     echo "Conexão falhou: " . $e->getMessage();
 }
 ?>
