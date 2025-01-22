109cc4e743ee366b48e9c0b3aefdd90ff42ee82e
Max Sidhartha Souza Rocha <max_rocha88@hotmail.com>
2025-01-22 12:41:05 -0300

    atulização 3 do backend edumonitor

--- config.php
+++ config.php
@@ -1,21 +1,22 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';  // Carrega as bibliotecas do Composer
+
+// Carrega o arquivo .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Obter as variáveis de ambiente
-$servername = getenv('MYSQLHOST');   // Host do banco de dados
-$username = getenv('MYSQLUSER');     // Usuário do banco de dados
-$password = getenv('MYSQLPASSWORD'); // Senha do banco de dados
-$dbname = getenv('MYSQLDATABASE');   // Nome do banco de dados
+$servername = $_ENV['MYSQLHOST'];   // Host do banco de dados
+$username = $_ENV['MYSQLUSER'];     // Usuário do banco de dados
+$password = $_ENV['MYSQLPASSWORD']; // Senha do banco de dados
+$dbname = $_ENV['MYSQLDATABASE'];   // Nome do banco de dados
 
 try {
     // Criar a conexão com PDO
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-
-    // Configurar o modo de erro do PDO para exceções
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Se a conexão for bem-sucedida
     echo "Conectado com sucesso!";
 } catch (PDOException $e) {
-    // Se houver erro na conexão
     echo "Conexão falhou: " . $e->getMessage();
 }
 ?>
