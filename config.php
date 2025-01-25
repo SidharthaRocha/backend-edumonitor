a81103111cc0b78640ca64368be9d82dfe408d67
Max Sidhartha Souza Rocha <max_rocha88@hotmail.com>
2025-01-25 17:44:27 -0300

    backend atualizadooo

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
-// Conexão remota com o banco de dados utilizando as variáveis de ambiente
-$host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
-$db = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
+// Configuração do banco de dados remoto no Railway
+$host = 'mysql.railway.internal'; // MYSQLHOST
+$db = 'railway'; // MYSQLDATABASE
+$user = 'root'; // MYSQLUSER
+$pass = 'BHfvIHTDwRyggmmHaNeFyTjJTwSexvum'; // MYSQLPASSWORD
+$port = 3306; // MYSQLPORT
 
-// Conexão com o banco de dados MySQL usando mysqli
-$conn = new mysqli($host, $user, $password, $db, $port);
-
-// Verifica se houve erro na conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-} else {
-    echo "Conexão bem-sucedida!";
+try {
+    // Conexão usando as configurações do Railway
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão bem-sucedida com o banco de dados!';
+} catch (PDOException $e) {
+    // Caso ocorra erro, retorna o status HTTP 500 e exibe a mensagem
+    echo 'Conexão falhou: ' . $e->getMessage();
+    http_response_code(500);
+    exit;
 }
-
-// Fechar a conexão
-$conn->close();
 ?>
+
